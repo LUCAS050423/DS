0dcea5325bd9488838a2b7c21bde31d3bccf838f
LUCAS050423 <145132132+LUCAS050423@users.noreply.github.com>
2024-03-20 08:14:37 -0300

    Add files via upload

---
+++ ExercicioImc/Imc.java
@@ -0,0 +1,34 @@
+package exe1;
+
+public class Imc {
+	  String nome;
+	  int idade;
+	  double peso;
+	  double altura;
+	  
+	  double calcular() {
+		  double imc = peso / (altura*altura);
+		  
+
+		  if (imc < 18.5) {
+			  System.out.println("Abaixo do peso");
+		  }
+		  else if (imc <=24.9) {
+			  System.out.println("Peso normal");
+		  }
+		  else if (imc <= 29.9) {
+			  System.out.println("Sobrepeso");
+		  }
+		  else if (imc <= 34.9) {
+			  System.out.println("Obesidade gral 1");
+		  }
+		  else if (imc <= 39.9) {
+			  System.out.println("Obesidade gral 2");
+		  }
+		  else {
+			  System.out.println("Obesidade gral 3");
+		  };
+		  
+	  return imc;
+	  }
+}
