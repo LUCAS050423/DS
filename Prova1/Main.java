9a0fb22a2a5f6dada2950e8fd4eb4011dcb81016
LUCAS050423 <145132132+LUCAS050423@users.noreply.github.com>
2024-05-29 01:40:40 -0300

    Update Main.java

--- Prova1/Main.java
+++ Prova1/Main.java
@@ -1,29 +1,14 @@
-
 public class Main {
-	public static void main(String[] args) {
-		
-		Livro livro = new Livro();
-		
-		livro.setAnoPubli(1954);
-		livro.setAutor("Roberto Carlos");
-		livro.setEditora("April");
-		livro.setNumeroPaginas(275);
-		livro.setTitulo("Chapeuzinho Vermelho");
-		
-		System.out.println(livro.getAnoPubli()+" /"+ livro.getAutor()+" /"+ livro.getEditora()+" /"+livro.getNumeroPaginas()+" /"+ livro.getTitulo());
-		
-		Revista revista = new Revista();
-		
-		revista.setAnoPubli(1954);
-		revista.setAutor("Roberto Carlos");
-		revista.setTitulo("Chapeuzinho Vermelho");
-		revista.setNumeroEdicao(333);
-		revista.setMesPublicacao("Março");
-		
-		System.out.println(revista.getAnoPubli()+" /"+revista.getAutor()+" /"+revista.getTitulo()+" /"+revista.getNumeroEdicao()+" /"+revista.getMesPublicacao());
-		
 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 		
+		 Livro livro=new Livro("Chapeuzinho Vermelho "," Roberto Carlos ", 1954 , 275 ," April ");
+		 System.out.println(livro.exibirDetalhes());
+		 
+		 
+		 Revista revista = new Revista("Chapeuzinho Vermelho","Roberto Carlos",1954,333,"março");
+		 System.out.println(revista.exibirDetalhes());
 	}
 
 }
