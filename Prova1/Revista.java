82dec96d74c83bf890576030c263e3a80cf81eb1
LUCAS050423 <145132132+LUCAS050423@users.noreply.github.com>
2024-05-29 01:42:01 -0300

    Update Revista.java

--- Prova1/Revista.java
+++ Prova1/Revista.java
@@ -1,22 +1,35 @@
+public class Revista extends Publicacao {
+	private int numEdicao;
+	private String mesEdicao;
+	
+	public Revista(String titulo,String autor,int anoPubli,int numEdicao,String mesEdicao) {
+		super(titulo,autor,anoPubli);
+		this.mesEdicao=mesEdicao;
+		this.numEdicao=numEdicao;
+		
+	}
 
-public class Revista extends Publicação{
-	private int numeroEdicao;
-	private String mesPublicacao;
-	
-	
-	public int getNumeroEdicao() {
-		return numeroEdicao;
+	public int getNumEdicao() {
+		return numEdicao;
 	}
-	public void setNumeroEdicao(int numeroEdicao) {
-		this.numeroEdicao = numeroEdicao;
+
+	public void setNumEdicao(int numEdicao) {
+		this.numEdicao = numEdicao;
 	}
-	public String getMesPublicacao() {
-		return mesPublicacao;
+
+	public String getMesEdicao() {
+		return mesEdicao;
 	}
-	public void setMesPublicacao(String mesPublicacao) {
-		this.mesPublicacao = mesPublicacao;
+
+	public void setMesEdicao(String mesEdicao) {
+		this.mesEdicao = mesEdicao;
 	}
-	
+
+	@Override
+	public String exibirDetalhes() {
+		return super.exibirDetalhes()+" Edição: "+ getNumEdicao()+" Publicação: "+getMesEdicao();
+		
+	}
 	
 
 }
