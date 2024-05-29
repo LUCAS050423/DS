6b706792d90834d5c64cfc87cd8dfe2a4e3b274f
LUCAS050423 <145132132+LUCAS050423@users.noreply.github.com>
2024-05-29 01:41:24 -0300

    Update Publicação.java

--- Prova1/Publicação.java
+++ Prova1/Publicação.java
@@ -1,29 +1,41 @@
+public class Publicacao {
 
-public class Publicação {
-	private String autor;
 	private String titulo;
-	private int AnoPubli;
+	private String autor;
+	private int anoPubli;
 	
+	public Publicacao(String titulo,String autor,int anoPubli) {
+		this.autor=autor;
+		this.titulo=titulo;
+		this.anoPubli=anoPubli;
+	}
+
 	public String getTitulo() {
 		return titulo;
 	}
+
 	public void setTitulo(String titulo) {
 		this.titulo = titulo;
 	}
+
 	public String getAutor() {
 		return autor;
 	}
+
 	public void setAutor(String autor) {
 		this.autor = autor;
 	}
+
 	public int getAnoPubli() {
-		return AnoPubli;
+		return anoPubli;
 	}
+
 	public void setAnoPubli(int anoPubli) {
-		AnoPubli = anoPubli;
+		this.anoPubli = anoPubli;
 	}
 	
-
-
-
+	public String exibirDetalhes() {
+		return "Titulo: "+titulo+"/ Autor: " + autor+"/ Publicação: " + anoPubli+" ";
+	}
+	
 }
