c2529032a5771d63c9bb9c8d66ef763eda90cd13
LUCAS050423 <145132132+LUCAS050423@users.noreply.github.com>
2024-05-29 01:37:51 -0300

    Update Livro.java

--- Prova1/Livro.java
+++ Prova1/Livro.java
@@ -1,23 +1,48 @@
 
-public class Livro extends Publicação{
-	private int numeroPaginas;
-	private String editora;
+public class Livro extends Publicacao {
+	 private int numPag;
+	 private String editora;
+	 
+	 
+	public Livro(String titulo,String autor,int anoPubli,int numPag,String editora) {
+		super(titulo,autor,anoPubli);
+		this.numPag=numPag;
+		this.editora=editora;
+		
+	}
+
+
+	public int getNumPag() {
+		return numPag;
+	}
+
+
+	public void setNumPag(int numPag) {
+		this.numPag = numPag;
+	}
 	
 
-	
-	public int getNumeroPaginas() {
-		return numeroPaginas;
-	}
-	public void setNumeroPaginas(int numeroPaginas) {
-		this.numeroPaginas = numeroPaginas;
-	}
+
 	public String getEditora() {
 		return editora;
 	}
+
+
 	public void setEditora(String editora) {
 		this.editora = editora;
 	}
+
+
+	@Override
+	public String exibirDetalhes() {
+		return super.exibirDetalhes()+"/ Numero de paginas: "+getNumPag()+"/ Editora "+getEditora();
+		
+	}
+
 	
+	
+	
+}
 
 
 }
