597ae39bf760406f42b2853396024b8085921042
LUCAS050423 <145132132+LUCAS050423@users.noreply.github.com>
2024-08-14 08:11:22 -0300

    Add files via upload

---
+++ Figuras/Main.java
@@ -0,0 +1,47 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+ 
+public class Main {
+    public static void main(String[] args) {
+Scanner scanner = new Scanner(System.in);
+        ArrayList<Figura> figuras = new ArrayList<>();
+ 
+        while (true) {
+            System.out.print("Digite a forma (circulo, retangulo) ou 'fim' para encerrar: ");
+            String tipo = scanner.next().toLowerCase();
+ 
+            if (tipo.equals("fim")) {
+                break;
+            }
+ 
+            switch (tipo) {
+                case "circulo":
+                    System.out.print("Digite o raio do círculo: ");
+                    double raio = scanner.nextDouble();
+                    figuras.add(new Circulo(raio));
+                    break;
+ 
+                case "retangulo":
+                    System.out.print("Digite a largura do retângulo: ");
+                    double largura = scanner.nextDouble();
+                    System.out.print("Digite a altura do retângulo: ");
+                    double altura = scanner.nextDouble();
+                    figuras.add(new Retangulo(largura, altura));
+                    break;
+ 
+                default:
+                    System.out.println("Forma inválida. Tente novamente.");
+                    break;
+            }
+        }
+        System.out.println("------------------------------------------------");
+ 
+        System.out.println("Áreas das figuras:");
+        for (Figura figura : figuras) {
+            System.out.println(figura.calcularArea());
+        }
+ 
+        scanner.close();
+    }
+}
