1feb7ab1968e4b52f8494ef863009fc8eee36e34
LUCAS050423 <145132132+LUCAS050423@users.noreply.github.com>
2024-10-09 06:22:26 -0300

    Add files via upload

---
+++ contas/Conta.java
@@ -0,0 +1,23 @@
+package contas;
+
+
+public abstract class Conta implements Operacoes {
+	
+	private double saldo = 0;
+	
+	public Conta(double saldo) {
+		this.saldo = saldo;
+	}
+	
+	public double getSaldo() {
+		return this.saldo;
+	}
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+	
+	public void verificarSaldo() {
+		System.out.println("Seu saldo é de R$ "+saldo);
+	}
+ 
+}
